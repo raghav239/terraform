6085aebb09d955b7a2fafe4c70b6d628f13279b8
raghav239 <pothularaghu02@gmail.com>
2023-03-15 18:13:30 +0530

    adding variables files

---
+++ variables/variables.tf
@@ -0,0 +1,11 @@
+variable content1 {
+  type        = string
+  default     = "I love terraform"
+  description = "content-test"
+}
+
+variable filename1 {
+  type        = string
+  default     = "variables.txt"
+  description = "filename"
+}
